9e67a2b1d9dda7a7376e3f6219bbd806d3abe402
Joseph Lee <facebooklee52@gmail.com>
2023-06-11 16:52:34 +0800

    Admin validation + all users backend

--- server/middleware/validateAdmin.js
+++ server/middleware/validateAdmin.js
@@ -1,20 +1,34 @@
 const { verify } = require('jsonwebtoken');
+const { User } = require("../models")
 require('dotenv').config();
-const validateToken = (req, res, next) => {
+const validateAdmin = async (req, res, next) => {
     try {
         const accessToken = req.header("Authorization").split(" ")[1];
         if (!accessToken) {
             return res.status(401).json({message: "JWT token is required"});
         }
         const payload = verify(accessToken, process.env.APP_SECRET);
+
         if (payload.type != "session") {
             return res.status(401).json({message: "JWT token is not the correct type"});
+        }
+        // Get user from database
+        const user = await User.findByPk(payload.user.email)
+        if (!user) {
+            return res.sendStatus(401);
+        }
+        // Check if user is admin
+        if (user.account_type != 0) {
+            return res.status(403).json({message: "You have no permission to access this resource"});
         }
         req.user = payload.user;
         return next();
     }
     catch (err) {
+        console.log(err)
         return res.status(401).json({message: "JWT token is missing or invalid"});
+        
     }
 }
-module.exports = { validateToken };
+
+module.exports = { validateAdmin };
