2c1e716f25f9729c3387685d3cff8fc72aec5e3a
Joseph Lee <facebooklee52@gmail.com>
2023-07-06 14:46:38 +0800

    Transaction detail page + fixes

---
+++ client/tproject/src/pages/profile/ViewTransactionHistoryDetails.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react'
+import { Box, Card, CardContent, Grid } from '@mui/material'
+import CardTitle from '../../components/CardTitle'
+import HistoryIcon from '@mui/icons-material/History';
+import { useParams } from 'react-router-dom';
+import http from '../../http'
+import InfoBox from '../../components/InfoBox';
+
+function ViewTransactionHistoryDetails() {
+
+    const [transaction, setTransaction] = useState({})
+    const [loading, setLoading] = useState(true)
+    const { id } = useParams()
+    const date = new Date()
+
+    const getTransaction = async () => {
+        setLoading(true)
+        const response = await http.get('/payment/history/' + id)
+        setTransaction(response.data)
+        setLoading(false)
+    }
+
+    useEffect(() => {
+        document.title = "EnviroGo - Transaction History"
+        getTransaction()
+    }, [])
+
+    return (
+        <>
+            <Card>
+                <CardContent>
+                    <CardTitle icon={<HistoryIcon />} title="Transaction Details" />
+                    <Grid container spacing={2} marginTop={"1rem"}>
+                        <Grid item xs={12} md={6}>
+                            <InfoBox title="Transaction ID" value={transaction.id} />
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <InfoBox title="Transaction Status" value={transaction.status} boolean={transaction.status == "Succeeded" ? true : false} />
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <InfoBox title="Transaction Date" value={date.toDateString(transaction.createAt)} />
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <InfoBox title="Transaction Type" value={transaction.type} />
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <InfoBox title="Last Updated" value={date.toTimeString(transaction.updatedAt)} />
+                        </Grid>
+                    </Grid>
+                </CardContent>
+            </Card>
+        </>
+    )
+}
+
+export default ViewTransactionHistoryDetails
