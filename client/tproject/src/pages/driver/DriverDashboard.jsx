c77900a68dc10d765531b3e701719ef24766618b
Brian <brianyuk@gmail.com>
2023-07-29 16:06:56 +0800

    Updated the driver routing page

--- client/tproject/src/pages/driver/DriverDashboard.jsx
+++ client/tproject/src/pages/driver/DriverDashboard.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
-import { Link, Box } from '@mui/material'
+import { Link, Box, Card, CardContent, Stack, Avatar, Typography, Grid, Container, Button, CardActions } from '@mui/material'
+import WalletIcon from '@mui/icons-material/Wallet';
+import DirectionsCarFilledIcon from '@mui/icons-material/DirectionsCarFilled';
+import SocialDistanceIcon from '@mui/icons-material/SocialDistance';
+import ReviewsIcon from '@mui/icons-material/Reviews';
 
 function DriverDashboard() {
   return (
@@ -9,7 +13,171 @@ function DriverDashboard() {
       flexDirection: 'column',
       alignItems: 'center'
     }}>
-    <Link href="/driver/routes">Driver routes</Link>
+      <Container maxWidth="xl" sx={{ marginTop: '2rem' }}>
+        <Grid container spacing={2} sx={{ marginBottom: "1.5em" }}>
+          <Grid item xs={12} xl={3} md={6} sm={6}>
+            <Card>
+              <CardContent>
+                <Stack
+                  alignItems="flex-start"
+                  direction="row"
+                  justifyContent="space-between"
+                  spacing={3}
+                >
+                  <Stack spacing={1}>
+                    <Typography
+                      color="text.secondary"
+                      variant="h5"
+                    >
+                      Wallet Balance
+                    </Typography>
+                    <Typography variant="h4">
+                      $432.32
+                    </Typography>
+                  </Stack>
+                  <Avatar
+                    sx={{
+                      backgroundColor: 'success.main',
+                      height: 56,
+                      width: 56
+                    }}
+                  >
+                    <WalletIcon />
+                  </Avatar>
+                </Stack>
+                <Typography variant="body2" color="text.secondary" sx={{ marginTop: '0.5rem' }}>
+                  This shows your wallet ballance from the profits made from driving others
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="large">Top-Up</Button>
+                <Button size="large">Withdraw</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={12} xl={3} md={6} sm={6}>
+            <Card>
+              <CardContent>
+                <Stack
+                  alignItems="flex-start"
+                  direction="row"
+                  justifyContent="space-between"
+                  spacing={3}
+                >
+                  <Stack spacing={1}>
+                    <Typography
+                      color="text.secondary"
+                      variant="h5"
+                    >
+                      Total Drives
+                    </Typography>
+                    <Typography variant="h4">
+                      12
+                    </Typography>
+                  </Stack>
+                  <Avatar
+                    sx={{
+                      backgroundColor: 'secondary.main',
+                      height: 56,
+                      width: 56
+                    }}
+                  >
+
+                    <DirectionsCarFilledIcon />
+
+                  </Avatar>
+                </Stack>
+                <Typography variant="body2" color="text.secondary" sx={{ marginTop: '0.5rem' }}>
+                  This shows your total driven routes and their details
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="large">View all</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={12} xl={3} md={6} sm={6}>
+            <Card>
+              <CardContent>
+                <Stack
+                  alignItems="flex-start"
+                  direction="row"
+                  justifyContent="space-between"
+                  spacing={3}
+                >
+                  <Stack spacing={1}>
+                    <Typography
+                      color="text.secondary"
+                      variant="h5"
+                    >
+                      Driven Distance
+                    </Typography>
+                    <Typography variant="h4">
+                      128KM
+                    </Typography>
+                  </Stack>
+                  <Avatar
+                    sx={{
+                      backgroundColor: '#6AC4FB',
+                      height: 56,
+                      width: 56
+                    }}
+                  >
+                    <SocialDistanceIcon />
+
+                  </Avatar>
+                </Stack>
+                <Typography variant="body2" color="text.secondary" sx={{ marginTop: '0.5rem' }}>
+                  This shows your total driven distance when on duty, keep it up!
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="large">Learn more</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={12} xl={3} md={6} sm={6}>
+            <Card>
+              <CardContent>
+                <Stack
+                  alignItems="flex-start"
+                  direction="row"
+                  justifyContent="space-between"
+                  spacing={3}
+                >
+                  <Stack spacing={1}>
+                    <Typography
+                      color="text.secondary"
+                      variant="h5"
+                    >
+                      Reviews
+                    </Typography>
+                    <Typography variant="h4">
+                      4.3 Stars
+                    </Typography>
+                  </Stack>
+                  <Avatar
+                    sx={{
+                      backgroundColor: '#ffc107',
+                      height: 56,
+                      width: 56
+                    }}
+                  >
+                    <ReviewsIcon></ReviewsIcon>
+                  </Avatar>
+                </Stack>
+                <Typography variant="body2" color="text.secondary" sx={{ marginTop: '0.5rem' }}>
+                  This shows your average review ratings from your customers
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="large">View all</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        </Grid>
+        <Link href="/driver/routes">Driver routes</Link>
+      </Container>
     </Box>
   )
 }
--- client/tproject/src/pages/driver/DriverRouting.jsx
+++ client/tproject/src/pages/driver/DriverRouting.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-import { GoogleMap, MarkerF, useLoadScript, useJsApiLoader, DirectionsRenderer, Autocomplete } from "@react-google-maps/api";
-import { Button, Container, Stack, Divider, Grid, Card, CardContent, Box, TextField } from '@mui/material';
+import { GoogleMap, MarkerF, useLoadScript, useJsApiLoader, DirectionsRenderer, Autocomplete, } from "@react-google-maps/api";
+import { Button, Container, Stack, Divider, Grid, Card, CardContent, Box, TextField, Typography } from '@mui/material';
 import { useMemo, useState, useRef, useEffect } from "react";
 import googleMapsReverseGeocoder from '../../googleMapsReverseGeocoder'
+import { CopyAllSharp } from '@mui/icons-material';
+import { useSnackbar } from 'notistack';
+
 
 function DriverRouting() {
   const [latitude, setlatitude] = useState(1.3521)
@@ -16,20 +19,113 @@ function DriverRouting() {
   const [directionsResponse, setDirectionsResponse] = useState(null)
   const [distance, setDistance] = useState('')
   const [duration, setDuration] = useState('')
-  const [address, setaddress] = useState('')
+  const [visibleRoutes, setVisibleRoutes] = useState();
+  const { enqueueSnackbar } = useSnackbar();
 
-  const originRef = useRef('')
-  const destinationRef = useRef('')
+  const originRef = useRef({})
+  const destinationRef = useRef({})
+
+  const test = [
+    {
+      id: 1,
+      pickUp: "Yio Chu Kang MRT station",
+      destination: "Ang Mo Kio Hub"
+    },
+    {
+      id: 2,
+      pickUp: "Marina Bay Sands",
+      destination: "Gardens by the Bay"
+    },
+    {
+      id: 3,
+      pickUp: "Yio Chu Kang MRT station",
+      destination: "Ang Mo Kio Hub"
+    },
+    {
+      id: 11,
+      pickUp: "Yio Chu Kang MRT station",
+      destination: "Ang Mo Kio Hub"
+    },
+  ];
+  function groupRidesByPickUp(rides, maxRidesPerList) {
+    const groupedRides = [];
+
+    // Iterate through each ride
+    rides.forEach((ride) => {
+      let addedToExistingList = false;
+
+      // Check if the current ride can be added to an existing list
+      for (let i = 0; i < groupedRides.length; i++) {
+        if (groupedRides[i][0].pickUp === ride.pickUp && groupedRides[i].length < maxRidesPerList) {
+          groupedRides[i].push(ride);
+          addedToExistingList = true;
+          break;
+        }
+      }
+
+      // If the ride was not added to an existing list, create a new list for it
+      if (!addedToExistingList) {
+        groupedRides.push([ride]);
+      }
+    });
+
+    return groupedRides;
+  }
+  function createSummaryList(groupedRides) {
+    const summaryList = [];
+
+    // Iterate through each group of rides with the same pickUp location
+    for (const rideGroup of groupedRides) {
+      const pickUpLocation = rideGroup[0].pickUp;
+      const wayPoints = []; // Create a new waypoints array for each group
+
+      // Populate the waypoints array for the current group
+      for (const ride of rideGroup) {
+        const wayPoint = {
+          location: ride.destination,
+          stopover: true,
+        };
+        wayPoints.push(wayPoint);
+      }
+
+      // Get the destination from the last waypoint
+      const destination = wayPoints[wayPoints.length - 1].location;
+
+      // Remove the last waypoint
+      wayPoints.pop();
+
+      // Extract the names from the rides in the group
+      const names = rideGroup.map((ride) => ride.rider_name);
+
+      // Create the summary object and add it to the summaryList
+      const summaryObject = {
+        pickUp: pickUpLocation,
+        wayPoints: wayPoints,
+        names: names,
+        destination: destination, // Add the destination attribute
+      };
+
+      summaryList.push(summaryObject);
+    }
+
+    return summaryList;
+  }
+
+  const handleDelete = (index) => {
+    const newRoutes = visibleRoutes.filter((_, i) => i !== index);
+    setVisibleRoutes(newRoutes);
+  };
 
 
-  function showPosition(position) {
+
+  async function showPosition(position) {
     setlatitude(position.coords.latitude)
     setlongtitude(position.coords.longitude)
     googleMapsReverseGeocoder.get("json?latlng=" + position.coords.latitude + "," + position.coords.longitude + "&key=" + import.meta.env.VITE_DRIVER_GOOGLE_API_KEY)
       .then((res) => {
         if (res.status === 200) {
-          setaddress(res.data.results[1].formatted_address)
-          originRef.current.value = address
+          originRef.current.value = res.data.results[1].formatted_address
+          console.log('address', originRef.current.value)
         } else {
           console.log("Address retrieval failed", res.status)
         }
@@ -37,23 +133,54 @@ function DriverRouting() {
   }
 
   const calculateRoute = async () => {
-    if (!originRef.current.value || !destinationRef.current.value) {
-      return
+    try {
+      if (!originRef.current.value || !destinationRef.current.value) {
+        return;
+      } else {
+        // eslint-disable-next-line no-undef
+        const directionsService = new google.maps.DirectionsService();
+        const results = await directionsService.route({
+          origin: originRef.current.value,
+          destination: destinationRef.current.value,
+          waypoints: [],
+          optimizeWaypoints: true,
+          // eslint-disable-next-line no-undef
+          travelMode: google.maps.TravelMode.DRIVING,
+        });
+        setDirectionsResponse(results);
+        setDistance(results.routes[0].legs[0].distance.text);
+        setDuration(results.routes[0].legs[0].duration.text);
+      }
+    } catch (error) {
+      console.error('Error calculating route:', error);
+      // Handle the error here, e.g., display an error message or take appropriate action
+      enqueueSnackbar("Please input a start destination", { variant: "error" })
     }
-    else {
+  };
+  
+
+  const configureDestination = async (waypoints, destination) => {
+    try {
       // eslint-disable-next-line no-undef
-      const directionsService = new google.maps.DirectionsService()
+      const directionsService = new google.maps.DirectionsService();
       const results = await directionsService.route({
         origin: originRef.current.value,
-        destination: destinationRef.current.value,
+        destination: destination,
+        waypoints: waypoints,
+        optimizeWaypoints: true,
         // eslint-disable-next-line no-undef
         travelMode: google.maps.TravelMode.DRIVING,
-      })
-      setDirectionsResponse(results)
-      setDistance(results.routes[0].legs[0].distance.text)
-      setDuration(results.routes[0].legs[0].duration.text)
+      });
+      setDirectionsResponse(results);
+      setDistance(results.routes[0].legs[0].distance.text);
+      setDuration(results.routes[0].legs[0].duration.text);
+    } catch (error) {
+      console.error('Error configuring destination:', error);
+      // Handle the error here, e.g., display an error message or take appropriate action
+      enqueueSnackbar("Please input a start destination", { variant: "error" })
     }
-  }
+  };
+  
 
   function clearRoute() {
     setDirectionsResponse(null)
@@ -68,12 +195,18 @@ function DriverRouting() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(showPosition);
     }
+    console.log('after')
+    const listsOfRides = groupRidesByPickUp(test, 3);
+    const listsOfRoutes = createSummaryList(listsOfRides);
+    setVisibleRoutes(listsOfRoutes);
+    console.log(listsOfRoutes)
+
   }, [])
 
   return (
     <>
       {!isLoaded && <div>Loading...</div>}
-      {isLoaded && 
+      {isLoaded &&
         <Box sx={{
           border: 'solid, black',
           display: 'flex',
@@ -82,8 +215,8 @@ function DriverRouting() {
         }}>
           <Container maxWidth="xl" sx={{ marginTop: '2rem' }}>
             <Grid container spacing={2}>
-              <Grid item xs={9} lg={9} md={7} sm={12}>
-    
+              <Grid item xs={12} lg={9} md={7} sm={12}>
+
                 <GoogleMap
                   center={center}
                   zoom={15}
@@ -96,16 +229,39 @@ function DriverRouting() {
                   }}
                   onLoad={map => setMap(map)}
                 >
-    
+
                   {directionsResponse && (
                     <DirectionsRenderer directions={directionsResponse} />
                   )}
                   <MarkerF position={center} />
                 </GoogleMap>
               </Grid>
-              <Grid item xs={3} lg={3} md={5} sm={12}>
+              <Grid item xs={12} lg={3} md={5} sm={12}>
                 <Card sx={{ marginBottom: '1rem' }}>
                   <CardContent>
+                    <Grid container spacing={2}>
+                      <Grid item xs={12}>
+                        <h3 style={{margin:0}}>Chosen Route Stats: </h3>
+                        <p style={{margin:0}}>Click on any of the route requests and their respective stats will be displayed</p>
+                      </Grid>
+                      <Grid item xs={6}>
+                        {distance && (
+                          <h5 style={{margin:0}}>Distance: {directionsResponse.routes[0].legs[0].distance.text}</h5>
+                        )}
+                      </Grid>
+                      <Grid item xs={6} >
+                        {duration && (
+                          <h5 style={{margin:0}}>Duration: {directionsResponse.routes[0].legs[0].duration.text}</h5>
+                        )}
+                      </Grid>
+                      <Grid item xs={6} >
+                        {distance && (
+                          <h4 style={{margin:0, color:'green'}}>Profit: ${(((directionsResponse.routes[0].legs[0].distance.value/1000) * 2) *0.65).toFixed(2)}</h4>
+                        )}
+                      </Grid>
+                    </Grid>
+                    <hr />
+                    <br />
                     <Grid container spacing={1}>
                       <Grid item xs={4}>
                         <Button variant='contained' color='secondary' onClick={clearRoute}>
@@ -116,7 +272,7 @@ function DriverRouting() {
                         <h4 style={{ margin: 0 }}>Set Custom route</h4>
                       </Grid>
                     </Grid>
-    
+
                     <Autocomplete>
                       <TextField
                         fullWidth
@@ -127,7 +283,7 @@ function DriverRouting() {
                         InputLabelProps={{ shrink: true }}
                       />
                     </Autocomplete>
-    
+
                     <Autocomplete>
                       <TextField
                         fullWidth
@@ -136,19 +292,7 @@ function DriverRouting() {
                         inputRef={destinationRef}
                       />
                     </Autocomplete>
-                    <Grid container spacing={2}>
-                      <Grid item xs={6}>
-                        {distance && (
-                          <h5>Distance: {directionsResponse.routes[0].legs[0].distance.text}</h5>
-                        )}
-                      </Grid>
-                      <Grid item xs={6} >
-                        {duration && (
-                          <h5>Duration: {directionsResponse.routes[0].legs[0].duration.text}</h5>
-                        )}
-                      </Grid>
-                    </Grid>
-    
+
                     <Grid container spacing={2}>
                       <Grid item xs={6}>
                         <Button variant='contained' onClick={calculateRoute}>Directions</Button>
@@ -165,22 +309,65 @@ function DriverRouting() {
                         </Button>
                       </Grid>
                     </Grid>
-    
+
                   </CardContent>
                 </Card>
-                <Card>
-                  <CardContent>
-                    Route 1
-                    by marry
-                  </CardContent>
-                </Card>
+                <div className="div" style={{ maxHeight: '500px', overflowY: 'auto' }}>
+                  {visibleRoutes.map((routeObj, index) => (
+                    <Card key={index} style={{ marginBottom: '10px' }}>
+                      <CardContent>
+                        <Typography variant="h6" gutterBottom>
+                          <b>Pick Up:</b> {routeObj.pickUp}
+                        </Typography>
+                        <Typography variant="body1" gutterBottom>
+                          <b>Riders:</b> {routeObj.names.join(', ')}
+                        </Typography>
+                        <Typography variant="body1" gutterBottom>
+                          <b>Waypoints:</b>
+                          {routeObj.wayPoints.map((wayPoint, i) => (
+                            <li key={i}>
+                              {wayPoint.location}
+                            </li>
+                          ))}
+                        </Typography>
+                        <Typography variant="body1" gutterBottom>
+                          <b>Destination:</b> {routeObj.destination}
+                        </Typography>
+
+                        <Grid container spacing={0} sx={{ marginTop: 1 }}>
+                          <Grid item xs={4} >
+                            <Button
+                              variant='contained' onClick={() => configureDestination(routeObj.wayPoints, routeObj.destination)} >
+                              Direct
+                            </Button>
+                          </Grid>
+                          <Grid item xs={4} >
+                            <Button
+                              variant='contained' color='success'
+                            >
+                              Accept
+                            </Button>
+                          </Grid>
+                          <Grid item xs={4} >
+                            <Button
+                              onClick={() => handleDelete(index)}
+                              variant='contained' color='error'
+                            >
+                              Reject
+                            </Button>
+                          </Grid>
+                        </Grid>
+                      </CardContent>
+                    </Card>
+                  ))}
+                </div>
               </Grid>
             </Grid>
           </Container>
         </Box>
       }
     </>
-    
+
   );
 
 }
